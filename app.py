c2b11add1456ac19c142f985bb48f9815d6ba39f
Kingsly54 <kingslyw@nycstudents.net>
2026-02-04 09:22:20 -0500

    2

---
+++ app.py
@@ -0,0 +1,23 @@
+'''def discount(age, isMember, isResident):
+    if age<12 or age>=65 or isMember==True or isResident==True:
+        print("qualified")
+    else: print("unqualified")
+discount(11, isMember=False, isResident=False)'''
+""" x=3
+y=float(3)
+print(x,y) """
+""" values = [1,2.23,5,7,2,30,15,12]
+print(values[7])
+for i in values:
+    print(i) """
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+input("Input a sentence")
+x=print("what")
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
