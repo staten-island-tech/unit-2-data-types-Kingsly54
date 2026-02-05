a0936976c49dc6297e8d4bb8517b091477c2758e
Kingsly54 <kingslyw@nycstudents.net>
2026-02-04 22:38:48 -0500

    everything

--- app.py
+++ app.py
@@ -1,11 +1,63 @@
-"""12< =<65 member resident"""
 
-
-def discount(age, ismember, isresident):
+'''def discount(age, ismember, isresident):
     if age>=65 or age<12 or ismember==True or isresident==True: 
         print("qualified")
     else:
         print("unqualified")
-discount(12, False, False)
+discount(12, False, False)'''
+'''x = 3
+y = float(3)
+print(x,y)
+'''
+'''values = [1,2.23,5,7,2,30,15,12]
+print(values)
+for i in values:
+    print(i)
+print(values[7])'''
 
 
+'''number=56
+if number % 2 == 0:
+    print("even")
+else: 
+    print("odd")'''
+'''bill=56
+Quality=input('Service quality')
+x="tippercentage"
+if Quality=="bad":
+    print(x)
+elif Quality=="okay":
+    print(x)
+    x=.15*bill
+elif Quality=="Good":
+    print(x)
+    x=.2*bill
+elif Quality=="Great":
+    print(x)
+    x=.25*bill
+Total=bill+x
+Quality="Great"
+print(Total)'''
+
+'''def factors(x):
+   print(x)
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+Number=int(input("What is your number"))
+factors(Number)'''
+def factors(x):
+   print(x)
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+Number=int("16")
+factors(Number)
+    
+
+
+
+
+    
